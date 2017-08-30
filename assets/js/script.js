8c986493e38e0b0ddaa555dbf8db2082ee625f2e
AishaLawal <noBeast21@mail.com>
2017-08-30 16:48:37 +0100

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$('*').addClass('animated');
+	introAnimation();
+	musicControls();
+});
+function musicControls(){
+	document.getElementById("song").volume = 0.6;
+}
+function introAnimation(){
+$('.intro').addClass('fadeInDown');
+
+
+	$('.fill').one('webkitAnimationEnd',function(){
+		setTimeout(function(){
+			$('.intro').addClass("fadeOutUp");
+			setTimeout(function(){
+				$('.intro').hide("slow");
+			},3000);
+
+
+			$('.total-logo').addClass("fadeInDown");
+
+
+		},2000);
+
+
+		setTimeout(function(){
+		$('.total-logo').addClass("fadeOutDown")
+		setTimeout(function(){
+			$('.total-logo').hide("slow");
+		},1000);
+	},6500);
+setTimeout(function(){
+	$('.saying').css('display','block');
+},12900);
+setTimeout(function(){
+	$('.saying').css('display','none');
+
+},14000);
+setTimeout(function(){
+	$('.southSound-heading').css('display','block');
+},15100);
+
+
+	});
+}
